376ef488cadc9d78c2459eafa438886c46188d59
kasseg <kapone86@mail.ru>
2019-06-25 10:53:35 +0300

    Add files via upload

---
+++ silprograms/URSA/ddd.js
@@ -0,0 +1,3 @@
+// var body = document.getElementsByClassName('view-issue-field editable-field inactive');
+// body[0].style.background = "url('/images/burning.gif') no-repeat center center";
+// body[0].style.backgroundColor  = "red";
